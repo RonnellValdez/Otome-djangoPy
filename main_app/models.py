b79350cc2a63cc9db13a1e7fc6dbbf6ef87b4610
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-06-08 10:41:02 -0700

    model for images

--- main_app/models.py
+++ main_app/models.py
@@ -2,3 +2,11 @@ from django.db import models
 
 # Create your models here.
 
+class Photos(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,9 @@ from django.views import View # <- View class to handle requests
 from django.http import HttpResponse # <- a class to handle sending a type of response  
 from django.views.generic.base import TemplateView
 
+#import models
+from .models import Photos
+
 # Create your views here.
 
 # Here we will be creating a class called Home and extending it from the View class
@@ -12,5 +15,11 @@ class Home(TemplateView):
 class Main(TemplateView):
     template_name = 'main.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["photos"] = Photos.objects.all()
+        return context
+
 class Sign_up(TemplateView):
     template_name = 'sign_up.html'
+
