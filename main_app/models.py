03ee3a9967ca62b63a6ff64c85e761c947fd1945
ronnellvjr <ronnell.b.valdez@gmail.com>
2021-06-08 15:19:06 -0700

    minor css fixes

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -13,3 +15,54 @@ class Photos(models.Model):
     def __str__(self):
         return self.name
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_picture = models.CharField(max_length=200)
+#     birth_date = models.DateField(null=True, default='1990-01-01', blank=True)
+#     gamer_tag = models.CharField(max_length=50)
+#     system = models.
+#     discord = models.CharField(max_length=50)
+#     twitch = models.CharField(max_length=200)
+#     bio = models.TextField(max_length=500, default='', blank=False)
+#     SYSTEM = (
+#         ('PC', 'PC'),
+#         ('XBOX', 'Xbox'),
+#         ('PLAYSTATION', 'Playstation'),
+#         ('SWITCH', 'Switch'),
+#         ('OTHER', 'Other'),
+#     )
+#     LOOKING_FOR = (
+#         ('MALE', 'Men'),
+#         ('FEMALE', 'Women'),
+#         ('BOTH', 'Both'),
+#     )
+#     APPROVAL = (
+#         ('TO BE APPROVED', 'To be approved'),
+#         ('APPROVED', 'Approved'),
+#         ('NOT APPROVED', 'Not approved')
+#     )
+#     GENDER = (
+#         ("MALE", "Male"),
+#         ("FEMALE", "Female")
+#         ("OTHER", "Other")
+#     )
+#     TIMEZONE = (
+#         ("EST", "Eastern Time"),
+#         ("CT", "Central Time")
+#         ("MT", "Mountain Time")
+#         ("PT", "Pacific Time")
+#     )    
+#     gender = models.CharField(choices=GENDER, default="OTHER", max_length=6)
+#     looking_for = models.CharField(choices=LOOKING_FOR, default='BOTH', blank=False, max_length=6)
+#     is_verified = models.CharField(choices=APPROVAL, default="TO BE APPROVED", blank=False, max_length=14)
+#     location = models.CharField(choices=TIMEZONE, default="PT", blank=False, max_length=5)
+#     system = models.CharField(choices=SYSTEM, default="OTHER", blank=False, max_length=20)
+
+# @receiver(post_save, sender=User)
+# def create_user_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender=User)
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
